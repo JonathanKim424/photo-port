0b9030329e8325e48711790a51b6969ed148daef
JonathanKim424 <vizard424@yahoo.com>
2022-06-16 16:10:13 -0600

    added form handling to contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    function handleChange(element) {
+        setFormState({...formState, [element.target.name]: element.target.value })
+    }
+
+    function handleSubmit(element) {
+        element.preventDefault();
+        element.stopPropagation();
+        console.log(formState);
+    }
+
     return (
         <section>
             <h1>Contact Me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" defaultValue={name} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" defaultValue={email} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <input type="message" rows="5" />
+                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
                 </div>
                 <button type="submit">Submit</button>
             </form>
